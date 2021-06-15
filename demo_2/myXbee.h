fce62059912fdbfc8f1b48b7c72ea80f0b97bad2
hinariEE <esugarmaple@gmail.com>
2021-06-15 15:43:51 +0800

    none

--- demo_2/myXbee.h
+++
@@ -1,7 +0,0 @@
-extern BufferedSerial xbee;
-
-void xbee_rx_interrupt(void);
-void xbee_rx(void);
-void reply_messange(char *xbee_reply, char *messange);
-void check_addr(char *xbee_reply, char *messenger);
-void xbee_init();
